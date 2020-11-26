b8715252f09454e8e587d48df37876d5283eeac3
USER4 <USER4@DESKTOP-9PUGOBF>
2020-11-25 18:48:53 -0800

    CHANGE PREETI

--- test/test/Controllers/HomeController.cs
+++ test/test/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace test.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your c  ontact page.";
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
